508f964f797a9823e3283e8ac918a21f3a0b0eef
Nandhini-2323 <nandhini.ace@gmail.com>
2024-12-09 19:18:18 +0530

    week2assignment2
    Chrome browser

---
+++ week2assign2/Chrome.java
@@ -0,0 +1,20 @@
+package week2assign2;
+
+public class Chrome {
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("The version of Chrome is " +version);
+		System.out.println("The developer of Chrome is " +developer);
+		System.out.println("The Chrome is Beta: " +isBeta);
+		System.out.println("The release year of Chrome is " +releaseYear);
+		System.out.println("The shortcut key for Chrome is " +shortcutKey);
+		
+			
+	}
+
+}
