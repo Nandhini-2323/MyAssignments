0f0f61e612053dfc31bd55c9885137a2740a479c
Nandhini-2323 <nandhini.ace@gmail.com>
2024-12-23 21:31:18 +0530

    Add files via upload
    added comment lines

--- homeAssignment/ChangeOddIndexToUppercase.java
+++ homeAssignment/ChangeOddIndexToUppercase.java
@@ -7,18 +7,23 @@ public class ChangeOddIndexToUppercase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String input ="changeme";
+		//converting the given string to character array
 		char[] characterInput = input.toCharArray();
-		//System.out.println(characterInput[3]);
+		
 		int length = characterInput.length;
-		//System.out.println(length);
+		
 		for(int i=0; i<=length; i++)
 		{
+			//finding the odd index
 			if(i%2 != 0)
 			{
+				//converting the odd index to uppercase Character class
+				//converting for one odd index at a time
 				characterInput[i]=Character.toUpperCase(characterInput[i]);
 				
 			}
 		}
+		//storing the converted character array to string
 		String result = new String(characterInput);
 		
 		System.out.println("The given string is "+input +" and the changed string is " +result);
