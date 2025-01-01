2740721b581bccb7452f5deca014dbad9f63b623
Nandhini-2323 <nandhini.ace@gmail.com>
2025-01-01 21:31:22 +0530

    Add files via upload

---
+++ listAssignment/SecondLargestNumber.java
@@ -0,0 +1,22 @@
+package listAssignment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SecondLargestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer Array[] = {3,2,11,4,6,7};
+		//Array to list
+		List<Integer> list = new ArrayList<>(Arrays.asList(Array));
+		Collections.sort(list);
+		int length = list.size();
+		
+		System.out.println("The second largest number is  "+list.get(4));
+
+	}
+
+}
